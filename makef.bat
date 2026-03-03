829441dc26a46a8f96b1dc686ed53718301044ff
Captain Chaos <40307525+burntcouch@users.noreply.github.com>
2026-03-03 10:48:54 -0700

    new working copy
    Renamed most of the ZP variables with longer, clearer names.  Added custom debugging.
    
    Also, agcb's milliForth assumes (apparently) that the ZP is all 0'd on start, so it doesn't bother to initialize it.  I added a routine to zero $50 - $FF on start.
    
    DEBUG := 1 enables debugging stuff.

--- makef.bat
+++ makef.bat
@@ -2,4 +2,4 @@ REM create msbasic for hydra
 REM
 del temp\*.o
 h:\cc65\bin\ca65.exe -l temp\millif.txt -o temp\hy_millif.o hy_millif.s
-h:\cc65\bin\ld65 -vm -C sector-6502.cfg temp\hy_millif.o -o temp\hy_millif.bin -Ln temp\hy_millif.lbl
+h:\cc65\bin\ld65 -vm -C hy_millif.cfg temp\hy_millif.o -o temp\hy_millif.bin -Ln temp\hy_millif.lbl
